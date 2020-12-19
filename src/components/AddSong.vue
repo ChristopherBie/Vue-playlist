7a59863acf4fbeb6a85b7c0803e69fe5b3bc08d6
Chris <newuser@News-MBP.hitronhub.home>
2020-12-19 09:02:44 -0700

    W13C

---
+++ src/components/AddSong.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="panel-block">
+        <form @submit.prevent="addNewSong">
+            <input type="text" placeholder="Artist" v-model="newArtist">
+            <input type="text" placeholder="Title" v-model="newTitle">
+            <button type="submit">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AddSong',
+        data() {
+            return {
+                newArtist: '',
+                newTitle: ''
+            }
+        },
+        methods: {
+            addNewSong() {
+                this.$emit('add', {
+                    artist: this.newArtist,
+                    title: this.newTitle
+                });
+                this.newArtist = '';
+                this.newTitle = '';
+                console.log(this.newArtist + ' - ' + this.newTitle);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
